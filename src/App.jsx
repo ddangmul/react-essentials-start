0cb2a0dc736c28bc6c217e80ddc60399112c4995
ddangmul <j.ddangmul@gmail.com>
2024-09-25 12:09:12 +0900

    이밴트함수에 매개변수/ 인수 전달 학습

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleSelect() {
-    console.log("Hello World - selected!");
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
 
   return (
@@ -24,10 +24,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("Components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
         </section>
       </main>
