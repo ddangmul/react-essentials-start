f94b72c747a5bc6042cd2757556262a7bbf04395
ddangmul <j.ddangmul@gmail.com>
2024-09-25 11:25:32 +0900

    리액트 이벤트처리 기초  학습

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton(prop) {
+export default function TabButton({ children, onSelect }) {
   return (
     <li>
-      <button>{prop.children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 }
